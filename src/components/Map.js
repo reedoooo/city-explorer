aa5214ab063c4f70aa348cb42fe3efb4045b8797
reedoooo <readvogt@gmail.com>
2023-02-25 10:03:14 -0800

    styles added and other bug fixes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,25 @@
+import { MDBContainer } from "mdb-react-ui-kit/dist";
 import React from "react";
-import { Container } from "react-bootstrap";
 
 class Map extends React.Component {
   render() {
     return (
-      <Container>
-        <img
-          src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.props.lat},${this.props.lon}&zoom=13&format=json`}
-          alt=""
-        />
-        {console.log(process.env.REACT_APP_LOCATION_KEY)}
-      </Container>
+      <MDBContainer>
+        <div class="bg-image hover-overlay ripple">
+          <img
+            src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.props.lat},${this.props.lon}&zoom=13&format=json`}
+            class="img-fluid rounded"
+            alt=""
+          />
+          <a class="ripple" href="#!">
+            {" "}
+            <div
+              class="mask"
+              style={{ backgroundcolor: "rgba(57, 192, 237, 0.2)" }}
+            ></div>
+          </a>
+        </div>
+      </MDBContainer>
     );
   }
 }
