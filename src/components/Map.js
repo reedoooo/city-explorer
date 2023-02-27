9904195cf29b5fa732828b071b2f998ed620e0ac
reedoooo <readvogt@gmail.com>
2023-02-26 17:01:25 -0800

    see

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,16 @@
+import { MDBContainer } from "mdb-react-ui-kit";
 import React from "react";
-import { Container } from "react-bootstrap";
 
 class Map extends React.Component {
   render() {
     return (
-      <Container>
+      <MDBContainer>
         <img
           src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.props.lat},${this.props.lon}&zoom=13&format=json`}
           alt=""
         />
         {console.log(this.props.lat)}
-      </Container>
+      </MDBContainer>
     );
   }
 }
