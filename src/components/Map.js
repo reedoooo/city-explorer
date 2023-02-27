31d5fbc3ce0bd8d83d4e13000c69e24e7a30d7fb
reedoooo <readvogt@gmail.com>
2023-02-26 17:17:20 -0800

    img

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,7 +8,7 @@ class Map extends React.Component {
         <img
           src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.props.lat},${this.props.lon}&zoom=13&format=json`}
           alt=""
-        />
+        className="maps" />
         {console.log(this.props.lat)}
       </MDBContainer>
     );
