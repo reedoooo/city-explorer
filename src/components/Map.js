0286e3820d7fc5c04716abbb4765cc4f36a69a5e
reedoooo <readvogt@gmail.com>
2023-02-26 20:23:55 -0800

    chang

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ import React from "react";
 class Map extends React.Component {
   render() {
     return (
-      <MDBContainer>
+      <MDBContainer className="mapContainer">
         <img
           src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.props.lat},${this.props.lon}&zoom=13&format=json`}
           alt=""
