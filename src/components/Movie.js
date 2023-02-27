51baac2f6809e99525af1e0a232cda1a2f975138
reedoooo <readvogt@gmail.com>
2023-02-26 17:20:51 -0800

    stuff

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,25 @@
+import { MDBCard, MDBCardBody, MDBContainer } from "mdb-react-ui-kit";
 import React from "react";
 
 class Movie extends React.Component {
   render() {
     return (
       <div>
-        <h3>{this.props.title}</h3>
-        <img src={this.props.poster_path} alt={this.props.title} />
-        <p>{this.props.overview}</p>
-        <p>Release Date: {this.props.released_on}</p>
-        <p>
-          Average votes: {this.props.average_votes} Total votes:{" "}
-          {this.props.total_votes}
-        </p>
-        <p>Rating: {this.props.popularity}</p>
+        <MDBContainer>
+          <MDBCard>
+            <MDBCardBody>
+              <h3>{this.props.title}</h3>
+              <img src={this.props.poster_path} alt={this.props.title} />
+              <p>{this.props.overview}</p>
+              <p>Release Date: {this.props.released_on}</p>
+              <p>
+                Average votes: {this.props.average_votes} Total votes:{" "}
+                {this.props.total_votes}
+              </p>
+              <p>Rating: {this.props.popularity}</p>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBContainer>
       </div>
     );
   }
